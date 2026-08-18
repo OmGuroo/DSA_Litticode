309171d7ed82002f5247f256dec2fb71f620d5cb
OmGuroo <omgaurav0702@gmail.com>
2026-08-19 01:05:37 +0530

    Time: 14 ms (43.09%) | Memory: 46.1 MB (20.60%) - LeetSync

--- 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.java
@@ -1,27 +1,40 @@
 class Solution {
-    public boolean isp(int n){
-        if(n == 1) return false;
-        for(int i=2; i * i<=n; i++){
-            if((n % i) == 0){
-                return false;
-            }
-        }
-        return true;
-    }
+
     public int nonSpecialCount(int l, int r) {
-        
-        int k = (int)Math.sqrt(l);
-        int sq = k*k;
-        int tc = 0;
-        while(sq <= r){
-            if(isp(k)){
-                if(sq >= l){
-                    tc++;
+        int k =(int) Math.sqrt(r);
+        boolean[] isp = new boolean[k+1];
+        Arrays.fill(isp, true);
+
+        isp[0] = false;
+        isp[1] = false;
+
+        for(int i=2; i<=k; i++){
+            if(isp[i]){
+                for(int j = i*i; j <= k; j+=i){
+                    isp[j] = false;
                 }
             }
-            k++;
-            sq = k*k;
-        } 
+        }
+        int tc = 0;
+        int i =(int) Math.ceil(Math.sqrt(l));
+        for(; i*i<= r; i++){
+            if(isp[i]){
+                tc++;
+            }
+        }
         return r + 1 - (tc + l);
-    }
+        }
 }
+
+
+
+
+    // public boolean isp(int n){
+    //     if(n == 1) return false;
+    //     for(int i=2; i * i<=n; i++){
+    //         if((n % i) == 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
